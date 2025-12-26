ad8351a6196f0c47ed684c160c00dd0186a41126
cazmat <93198094+cazmat@users.noreply.github.com>
2025-12-26 11:47:56 -0600

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -1,7 +1,9 @@
 <?php
   $settings = [
+    "admin_email" => "me@mlutz.us",
     "base_url" => "http://localhost",
     "blog_ppp" => 10, // Blog Posts per Page.
+    "maintenance" => false,
     "pretty_url" => true,
     "site_name" => "My Blog"
   ];
